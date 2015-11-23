f7892eaf8684d4d9494b13ed6a46a1e524b5d87f
jonny.crespo <cresposoft@gmail.com>
2015-11-23 12:04:31 -0400

    adicion de nuevos archivos
    son archivos de la vista para cambiar en un futuro el login form

---
+++ src/main/java/bo/buffalo/model/GrupoProducto.java
@@ -0,0 +1,124 @@
+package bo.buffalo.model;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="grupo_producto"
+,schema="public"
+)
+public class GrupoProducto implements Serializable {
+	private static final long serialVersionUID = 1L;
+	private Integer id;
+	private String descripcion;
+	private String estado;
+	private Date fechaRegistro;
+	private String usuarioRegistro;
+	
+
+	public GrupoProducto() {
+	}
+	
+	public GrupoProducto(String descripcion,String estado,String sigla){
+		this.descripcion=descripcion;
+		this.estado=estado;
+	}
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	public Integer getId() {
+		return this.id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+
+	public String getDescripcion() {
+		return this.descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion.toUpperCase();
+	}
+
+
+	public String getEstado() {
+		return this.estado;
+	}
+
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+
+
+	@Column(name="fecha_registro")
+	public Date getFechaRegistro() {
+		return this.fechaRegistro;
+	}
+
+	public void setFechaRegistro(Date fechaRegistro) {
+		this.fechaRegistro = fechaRegistro;
+	}
+
+
+	@Column(name="usuario_registro")
+	public String getUsuarioRegistro() {
+		return this.usuarioRegistro;
+	}
+
+	public void setUsuarioRegistro(String usuarioRegistro) {
+		this.usuarioRegistro = usuarioRegistro;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+		GrupoProducto other = (GrupoProducto) obj;
+		if (id == null) {
+			if (other.id != null) {
+				return false;
+			}
+		} else if (!id.equals(other.id)) {
+			return false;
+		}
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "TipoProducto [id=" + id + ", descripcion=" + descripcion
+				+ ", estado=" + estado + ", fechaRegistro=" + fechaRegistro
+				+ ", usuarioRegistro=" + usuarioRegistro
+				+ "]";
+	}
+
+	
+	
+
+
+}
